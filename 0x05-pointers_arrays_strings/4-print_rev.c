2dc10c5721237b4883bddbfb61db00f4c525d5c2
Milton8086 <chidukspty@gmail.com>
2023-02-27 07:09:09 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,19 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
+	int count = 0;
 
-	while (*s != '0\')
+	while (*s)
 	{
-		longi++;
 		s++;
+		count++;
 	}
 
-	s--;
-
-	for (o = longi; o > 0; o--)
+	while (count)
 	{
+		s--;
 		_putchar(*s);
-		s--
+		count--;
 	}
 
 	_putchar('\n');
