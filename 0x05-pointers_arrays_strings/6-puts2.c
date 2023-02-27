e4dd81c043393320e18a39bd6c05d28febb8ca91
Milton8086 <chidukspty@gmail.com>
2023-02-27 07:35:41 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every othr character of a string, starting with the \
+ * first charater, followed by a new line,
+ * @str: sring to be printed
+ *
+ * Return: nothing on succes
+ */
+
+void puts2(char *str)
+{
+	int count = 0, i;
+
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+
+	for (i = 0; i < count; i++)
+
+	for (i = 0; i < count; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
