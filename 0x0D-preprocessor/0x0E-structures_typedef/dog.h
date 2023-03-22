b63041d490e926a0e1b1e79476e998f08f09dd6f
Milton8086 <chiduskpty@gmail.com>
2023-03-22 02:29:59 -0700

    dog.h

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,20 +1,33 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_FILE
+#define DOG_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *struct dog - a new type
- *@name : string
- *@age : float
- *@owner : string
- *Description: a new type that contains dog name, age, and owner.
+ * struct dog - a new type
+ * @name: name
+ * @age: the dog age
+ * @owner: name of dog owner
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+
+#endif /* DOG_FILE */
