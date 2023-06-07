e71adf3d08e5ba712ec71b10198556ec9af8ccf5
Milton8086 <chidukspty@gmail.com>
2023-06-07 12:48:31 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,23 @@
 #include "main.h"
 
 /**
- * int _strlen_recursion-
+ * _strlen_recursion- a function that returns the length of a string
+ * @s:character to be counted
+ *
+ * Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	/*check for the null character*/
+
+	if (*s != '\0')
+	{
+		return 0;
+	}
+	/*if not a character return 1 and call function again*/
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
